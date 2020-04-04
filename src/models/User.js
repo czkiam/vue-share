41d4f4b3243c549b0ace92fb0a1e2a87b803244c
czkiam <czkiam@gmail.com>
2020-04-05 03:41:45 +0800

    implement hash for password with bcrypt

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const md5 = require("md5");
+const bcryt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -25,6 +27,31 @@ const UserSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Post"
   }
+});
+
+//Create and add avatar to user
+UserSchema.pre("save", function(next) {
+  this.avatar = `http://gravatar.com/avatar/${md5(this.username)}?d=identicon`;
+  next();
+});
+
+//hash password so it cant be seen
+UserSchema.pre("save", function(next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  bcryt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcryt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      console.log(hash);
+      this.password = hash;
+      next();
+    });
+  });
 });
 
 module.exports = mongoose.model("User", UserSchema);
